3ab21c736f02f3dce1450b54eb8281920e8387c6
Timo Strating <timostrating@home.nl>
2018-03-22 00:58:39 +0100

    first test

--- tentamens/index.php
+++ tentamens/index.php
@@ -1,13 +1,25 @@
+<pre>
 <?php
 
 require 'vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 
-$spreadsheet = new Spreadsheet();
-$sheet = $spreadsheet->getActiveSheet();
-$sheet->setCellValue('A1', 'Hello World !');
 
-$writer = new Xlsx($spreadsheet);
-$writer->save('hello world.xlsx');
+$file ='test.xlsx';
+$spreadsheet = IOFactory::load($file);
+
+$xls_data = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+
+// var_dump($xls_data);
+
+$html_tb ='<table border="1"><tr><th>'. implode('</th><th>', $xls_data[1]) .'</th></tr>';
+$nr = count($xls_data); //number of rows
+for($i=2; $i<=$nr; $i++){
+  $html_tb .='<tr><td>'. implode('</td><td>', $xls_data[$i]) .'</td></tr>';
+}
+$html_tb .='</table>';
+
+echo $html_tb;
