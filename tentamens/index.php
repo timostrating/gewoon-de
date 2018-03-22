a0fb88b043c84b2683d207d859d209db30791d3e
Timo Strating <timostrating@home.nl>
2018-03-22 19:48:52 +0100

    sortable tables

--- tentamens/index.php
+++ tentamens/index.php
@@ -1,4 +1,22 @@
-<pre>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>gewoon.de</title>
+        <script src="sorttable.js"></script>
+        <style>
+            /* Sortable tables */
+            table.sortable thead {
+                background-color:#eee;
+                color:#666666;
+                font-weight: bold;
+                cursor: default;
+            }
+        </style>
+    </head>
+    <body>
+        <pre>
+
+
 <?php
 
 require 'vendor/autoload.php';
@@ -12,14 +30,58 @@ $file ='test.xlsx';
 $spreadsheet = IOFactory::load($file);
 
 $xls_data = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+$show = [
+    "O", // Klas
+    // "A", // V/D
+    // "B", // D/B
+    "C", // Periode
+    "D", // Soort
+    // "E", // Type
+    // "F", // Opleiding
+    "G", // Weeknr
+    "H", // Datum
+    "I", // Van
+    "J", // Tot
+    // "K", // vakcode
+    "L", // Omschrijving
+    // "M", // Toets_Oms
+    // "N", // NULL
+    // "P", // Duur
+    "Q", // Lokatie
+    "R"  // Bijzonderheden
+];
 
 // var_dump($xls_data);
+?>
 
-$html_tb ='<table border="1"><tr><th>'. implode('</th><th>', $xls_data[1]) .'</th></tr>';
-$nr = count($xls_data); //number of rows
-for($i=2; $i<=$nr; $i++){
-  $html_tb .='<tr><td>'. implode('</td><td>', $xls_data[$i]) .'</td></tr>';
-}
-$html_tb .='</table>';
+            <table class="sortable" border="1">
+                <tr>
+                    <?php for($i=0; $i < count($show); $i++) { ?>
+                        <th> <?= $xls_data[1][$show[$i]] ?> </th>
+                    <?php } ?>
+                </tr>
 
-echo $html_tb;
+                <?php for($i=2; $i <= count($xls_data); $i++) {  if( $xls_data[$i]["O"] !== "Jaar 1") continue ?>
+                    <tr>
+                        <?php for($j=0; $j < count($show); $j++) { ?>
+                            <th> <?= $xls_data[$i][$show[$j]] ?> </th>
+                        <?php } ?>
+                    </tr>
+                <?php } ?>
+
+            </table>
+
+
+
+
+
+
+            <table border="1"><tr><th> <?= implode('</th><th>', $xls_data[1]) ?> </th></tr>
+                <?php for($i=2; $i <= count($xls_data); $i++) { ?>
+                    <tr><td> <?= implode('</td><td>', $xls_data[$i]) ?> </td></tr>
+                <?php } ?>
+            </table>
+
+        </pre>
+    </body>
+</html>
