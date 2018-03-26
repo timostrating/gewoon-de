0169f102a090ec184e349a7a17920a2de2b1cf33
Timo Strating <timostrating@home.nl>
2018-03-26 20:42:42 +0200

    added tetamens and memes to the list

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ $links = array(
     "whatsapp" => "https://chat.whatsapp.com/invite/Hcz6GtqxJ8Z0C5NuUDhIrI", 
     "boeken" => "https://drive.google.com/drive/folders/0B3QX7v_nrvn9RjNDVWJUTVpWZzQ?usp=sharing", 
     "samenvatting"=>"https://drive.google.com/drive/folders/1WdroHuz2JB9LXdveRVX25hGktHCHudfx?usp=sharing",
-    "memes"=>"https://www.reddit.com/r/Hanzememes/"
+    "memes"=>"https://www.reddit.com/r/Hanzememes/",
+    "tentamens" => "/tentamens"
 );
 
 if (array_key_exists($_SERVER['REQUEST_URI'], $links)) {
@@ -19,9 +20,9 @@ if (array_key_exists($_SERVER['REQUEST_URI'], $links)) {
 </head>
 <body>
 
-<?php foreach ($links as $key => $value) {
-		echo '<a href="'.$key.'">'.$key.'</a> <br/>';
-	} ?>
+    <?php foreach ($links as $key => $value) {
+        echo '<a href="'.$key.'">'.$key.'</a> <br/>';
+    } ?>
 
 </body>
 </html>
